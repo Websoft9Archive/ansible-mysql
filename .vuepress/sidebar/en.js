9f62e4ed2656ecaf8471e24b2fa4fdfdc1e3f15c
darren chen <chendelin1982@126.com>
2020-01-26 21:30:00 +0800

    html

---
+++ .vuepress/sidebar/en.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    title: 'Getting Started',
+    collapsable: false,
+    children: [
+      '/',
+      '/stack-deployment',
+      '/stack-installation',
+      '/stack-components',
+      '/stack-accounts',
+      ['https://support.websoft9.com/docs/faq/tech-instance.html','Cloud Server'],
+    ]
+  },
+  {
+    title: 'Best Practices',
+    collapsable: false,
+    children: [
+      '/solution-study',
+      '/solution-password',
+      '/solution-remote',
+      '/solution-phpmyadmin',
+      '/solution-jspmyadmin',
+      '/solution-navicat',
+      '/solution-workbench',
+      '/solution-mysqlfront',
+      '/solution-backup',
+      '/solution-upgrade',
+      '/solution-more',
+    ]
+  },
+  
+  {
+    title: 'Administrator',
+    collapsable: false,
+    children: [
+      '/admin-services',
+      ['https://support.websoft9.com/docs/linux/admin-practices.html','Linux'],
+      ['https://support.websoft9.com/docs/windows/admin-practices.html','Windows'],
+    ]
+  },
+  
+  {
+    title: 'Other',
+    collapsable: false,
+    children: [
+      '/else-faq',
+      '/else-troubleshooting',
+    ]
+  },
+  
+]
---
+++ .vuepress/sidebar/zh.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    title: '开始',
+    collapsable: false,
+    children: [
+      '/zh/',
+      '/zh/stack-deployment',
+      '/zh/stack-installation',
+      '/zh/stack-components',
+      '/zh/stack-accounts',
+      ['https://support.websoft9.com/docs/faq/zh/tech-instance.html','云服务器'],
+    ]
+  },
+  
+  {
+    title: '实践指南',
+    collapsable: false,
+    children: [
+      '/zh/solution-study',
+      '/zh/solution-password',
+      '/zh/solution-remote',
+      '/zh/solution-phpmyadmin',
+      '/zh/solution-jspmyadmin',
+      '/zh/solution-navicat',
+      '/zh/solution-workbench',
+      '/zh/solution-mysqlfront',
+      '/zh/solution-backup',
+      '/zh/solution-upgrade',
+      '/zh/solution-more',
+    ]
+  },
+  
+  {
+    title: '配置参考',
+    collapsable: false,
+    children: [
+      '/zh/admin-services',
+      ['https://support.websoft9.com/docs/linux/zh/admin-practices.html','Linux'],
+      ['https://support.websoft9.com/docs/windows/zh/admin-practices.html','Windows'],
+    ]
+  },
+  
+  {
+    title: '其他',
+    collapsable: false,
+    children: [
+      '/zh/else-faq',
+      '/zh/else-troubleshooting',     
+    ]
+  },
+  
+  ]
